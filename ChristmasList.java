2d74b99db212651e165aba2c29c92840a28ec6d1
RyanMajd <58677980+RyanMajd@users.noreply.github.com>
2022-12-29 22:31:16 -0500

    Update ChristmasList.java
    added better comments

--- ChristmasList.java
+++ ChristmasList.java
@@ -2,18 +2,28 @@ import java.util.Scanner;
 public class ChristmasList {
 
 	public static void main(String[] args) {
+		//list of each item name
 		String[] cList = {"Hats!!!", "Game NBA Men's Hawks Jogger Sweatpants (M)", "SKATER KAERU Classic Tee", "AE AirFlex+ Original Straight Jean", "WILSON Evolution Game Basketball", "MURRAY PEACH NIKE CITY EDITION SWINGMAN (L)", "TRAE YOUNG 2.0 BASKETBALL SHOES (Light Blue)", "Apple Airpods Pro 2nd Gen"};
 		Double[] priceList = {20.0, 22.0, 23.0, 30.0, 75.0, 130.0, 140.0, 200.0};
 		double sumAmt = 0;
+		
+		
+		//for loop involved in the sumation of all price values
 		for (int i=0; i < cList.length; i++) {
 			sumAmt += priceList[i];
 		} 
+		
+		//output line (introduction)
 		System.out.println("Wassup Family!\n"
 				+ "First, please choose your budget for Ryan's Christmas present:\n"
 				+ "(entering a budget greater than $640.0 will output the whole list)\n$");
-		
+		//Initialization of Scanner Object named "scan"
 		Scanner scan = new Scanner(System.in);
+		//program asks for input in the form of a Double in order to determine what items can be bought with the balance given
 		double amt = scan.nextDouble();
+		
+		//constraints dependent on balance amount (Keep in mind this program was initially for my family, 
+		//so the crude humour is not something I would put in professional code
 		if (amt < 20.0) {
 			System.out.println("Uh oh! You are too poor to afford my present! Might as well just keep the money at that point, you need"
 					+ " it more than me!");
@@ -30,16 +40,19 @@ public class ChristmasList {
 
 		int index =0;
 		int maxAmt = 0;
+			//for loop which finds the most expensive item that the user can afford on the list. 
 		for (int i=0; i < priceList.length; i++) {
 			if (amt >= priceList[i]) {
 				maxAmt = priceList[i].intValue();
 				index = i;
 			}
 		}
+			//with the value of the index location from the for loop, 
+			//sets two variables which stores a String of the affordable
+			//and maximum priced item as well as a Double which is the amount
+			//of money left after purchasing the gift
 		String maxPresent = cList[index];
 		Double amtLeft = amt -maxAmt;
-		
-		
 		
 		// cheaper options
 		String [] cheaperOptions = new String[index-1];
